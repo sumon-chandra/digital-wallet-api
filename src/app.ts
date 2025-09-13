519102a29b122531284d6907c8a2174cf75704ce
Sumon Chandra <80484412+sumon-chandra@users.noreply.github.com>
2025-09-13 17:27:56 +0600

    update app config

--- src/app.ts
+++ src/app.ts
@@ -1,27 +1,27 @@
 import express, { Request, Response } from "express";
 import cors from "cors";
 import cookieParser from "cookie-parser";
-import expressSession from "express-session";
+// import expressSession from "express-session";
 import { router } from "./routes";
 import { globalErrorHandler } from "./middlewares/global-error-handler";
 
 const app = express();
-app.use(
-	expressSession({
-		secret: "secret",
-		resave: false,
-		saveUninitialized: false,
-	})
-);
+// app.use(
+// 	expressSession({
+// 		secret: "secret",
+// 		resave: false,
+// 		saveUninitialized: false,
+// 	})
+// );
 app.use(
 	cors({
 		origin: ["http://localhost:5173", "https://digital-wallet-client-two.vercel.app"],
 		credentials: true,
 	})
 );
+app.use(cookieParser());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(cookieParser());
 app.use("/api/v1", router);
 
 app.use(globalErrorHandler);
