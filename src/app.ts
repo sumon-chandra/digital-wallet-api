1ee0a80bf14a6637b2d572c5da35c29347f10685
Sumon Chandra <80484412+sumon-chandra@users.noreply.github.com>
2025-09-13 22:08:38 +0600

    update cors config

--- src/app.ts
+++ src/app.ts
@@ -1,30 +1,21 @@
 import express, { Request, Response } from "express";
 import cors from "cors";
 import cookieParser from "cookie-parser";
-// import expressSession from "express-session";
 import { router } from "./routes";
 import { globalErrorHandler } from "./middlewares/global-error-handler";
 
 const app = express();
-// app.use(
-// 	expressSession({
-// 		secret: "secret",
-// 		resave: false,
-// 		saveUninitialized: false,
-// 	})
-// );
-// app.use(
-// 	cors({
-// 		origin: ["https://digital-wallet-client-two.vercel.app", "http://localhost:5173/"],
-// 		credentials: true,
-// 	})
-// );
-app.options("*", cors());
+
+app.use(
+	cors({
+		origin: ["http://localhost:5173", "https://digital-wallet-client-two.vercel.app"],
+		credentials: true,
+	})
+);
 app.use(cookieParser());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use("/api/v1", router);
-
 app.use(globalErrorHandler);
 
 app.get("/", (req: Request, res: Response) => {
